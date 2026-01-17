2bd9902261b00affd5155afada9ed0890d736c13
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-01-18 00:16:33 +0100

    adding basic index.html files

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+const port = ":8080"
+
 func main() {
 
-	serverHandler := http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/asstes/", http.FileServer(http.Dir("./assets")))
 
 	server := http.Server{
-		Handler: serverHandler,
-		Addr:    ":8080",
+		Handler: mux,
+		Addr:    port,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Stopped Server: %v", err)
-	}
+	fmt.Printf("Starting Server on Port %v\n", port[1:])
+	log.Fatal(server.ListenAndServe())
 
 }

75a0f705fff86e30bc660a61ddaa18b333cba235
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-01-18 13:51:31 +0100

    refactor code to multiple files

---
+++ handler_reset.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.fileserverHits.Swap(0)
+	w.Write([]byte("Hits reset to 0"))
+
+}
--- main.go
+++ main.go
@@ -7,66 +7,33 @@ import (
 	"sync/atomic"
 )
 
-const port = ":8080"
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+const port = "8080"
 const filepathRoot = "."
 
 func main() {
-
-	mux := http.NewServeMux()
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux := http.NewServeMux()
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		w.Write([]byte("OK"))
-	})
-
-	mux.HandleFunc("/metrics", apiCfg.handlerMetric)
-
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetric)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Handler: mux,
-		Addr:    port,
+		Addr:    ":" + port,
 	}
 
-	fmt.Printf("Server is running on port %v\n", port[1:])
+	fmt.Printf("Server is running on port %v\n", port)
 	log.Fatal(server.ListenAndServe())
-
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerMetric(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-
-	line := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
-	w.Write([]byte(line))
-
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-
-	cfg.fileserverHits.Swap(0)
-	w.Write([]byte("OK"))
 
 }

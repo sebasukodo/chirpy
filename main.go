290509603bdd3dee3a0849e6e7a9e41d1ce29b42
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-01-18 12:06:15 +0100

    adding request counter to localhost:8080/metrics

--- main.go
+++ main.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = ":8080"
+const filepathRoot = "."
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.handlerMetric)
+
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Handler: mux,
@@ -27,5 +37,36 @@ func main() {
 
 	fmt.Printf("Server is running on port %v\n", port[1:])
 	log.Fatal(server.ListenAndServe())
+
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetric(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	line := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(line))
+
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	cfg.fileserverHits.Swap(0)
+	w.Write([]byte("OK"))
 
 }

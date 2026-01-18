9cdafcddbefacc0313bfb4b0787d4ef76732f71d
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-01-18 11:05:58 +0100

    adding custom http.Handler

--- main.go
+++ main.go
@@ -11,15 +11,21 @@ const port = ":8080"
 func main() {
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/asstes/", http.FileServer(http.Dir("./assets")))
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    port,
 	}
 
-	fmt.Printf("Starting Server on Port %v\n", port[1:])
+	fmt.Printf("Server is running on port %v\n", port[1:])
 	log.Fatal(server.ListenAndServe())
 
 }
